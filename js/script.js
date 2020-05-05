7b204074796a48a3b8fa8c69d53e629b273685a4
Yu Hayakawa <chelmilk@gmail.com>
2020-05-05 21:07:23 +0900

    update contents

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// PhotoSwipe
+initPhotoSwipeFromDOM('.my-gallery');
+
+$(function () {
+
+  $('.card-caption').on('click', 'a', function (e) {
+    e.stopPropagation();
+  });
+
+  var $nav = $('.gnav');
+  var navHeight = $nav.outerHeight();
+
+  //ページ内スクロール
+  $('a[href^="#"]').on('click', function () {
+    var speed = 300;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+
+  //ページトップへもどる
+  $('#js-page-top').on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+});
